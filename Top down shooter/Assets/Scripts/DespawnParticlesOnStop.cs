2629ed382135ce0806c838e7c0508db4dde2aef1
penguinkaleidoscop <penguinkaleidoscop@gmail.com>
2017-07-14 12:36:04 -0400

    Busting Barrels and Particle Effects
    There is now a broken version of the barrel.
    (It does not currently replace regular barrels when they are shot)
    There are now particle effects for the shot and the impact when firing.

---
+++ Top down shooter/Assets/Scripts/DespawnParticlesOnStop.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DespawnParticlesOnStop : MonoBehaviour
+{
+    private ParticleSystem ps;
+    
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+	void Update ()
+    {
+		if(ps.isStopped)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
